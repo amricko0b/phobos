f612cc3f844da71e43499c98f044ca3a6f0a4b55
valentiay <valentiay@yandex.ru>
2021-11-29 00:57:46 +0300

    Common methods in separate file

--- modules/core/src/main/scala-3/ru/tinkoff/phobos/derivation/encoder.scala
+++ modules/core/src/main/scala-3/ru/tinkoff/phobos/derivation/encoder.scala
@@ -4,6 +4,7 @@ import ru.tinkoff.phobos.Namespace
 import ru.tinkoff.phobos.configured.ElementCodecConfig
 import ru.tinkoff.phobos.syntax.*
 import ru.tinkoff.phobos.encoding.*
+import ru.tinkoff.phobos.derivation.common.*
 
 import scala.compiletime.*
 import scala.deriving.Mirror
@@ -13,155 +14,43 @@ import scala.compiletime.{erasedValue, summonInline}
 
 object encoder {
 
-  sealed trait FieldCategory
+  inline def deriveElementEncoder[T](
+    inline config: ElementCodecConfig
+  ): ElementEncoder[T] =
+    ${deriveElementEncoderImpl('{config})}
 
-  object FieldCategory {
-    case object element   extends FieldCategory
-    case object attribute extends FieldCategory
-    case object text      extends FieldCategory
-    case object default   extends FieldCategory
+  inline def deriveXmlEncoder[T](
+    inline localName: String,
+    inline namespace: Option[String],
+    inline config: ElementCodecConfig
+  ): XmlEncoder[T] =
+    ${deriveXmlEncoderImpl('{localName}, '{namespace}, '{config})}
+
+  def deriveElementEncoderImpl[T: Type](config: Expr[ElementCodecConfig])(using Quotes): Expr[ElementEncoder[T]] = {
+    import quotes.reflect.*
+
+    val tpe = TypeRepr.of[T]
+    val typeSymbol = tpe.typeSymbol
+    if (typeSymbol.flags.is(Flags.Case)) {
+      deriveProduct(config)
+    } else if (typeSymbol.flags.is(Flags.Sealed)) {
+      deriveSum(config)
+    } else {
+      report.throwError(s"${typeSymbol} is not a sum type or product type")
+    }
   }
 
-  final class CaseClassField(using val quotes: Quotes)(
-    val localName: String,
-    val xmlName: Expr[String], // Name of element or attribute
-    val namespaceUri: Expr[Option[String]],
-    val paramType: quotes.reflect.TypeRepr,
-    val category: FieldCategory,
-  )
-
-  final class SealedTraitChild(using val quotes: Quotes)(
-     val xmlName: Expr[String], // Value of discriminator
-     val subtypeType: quotes.reflect.TypeRepr,
-  )
+  def deriveXmlEncoderImpl[T: Type](
+    localName: Expr[String],
+    namespace: Expr[Option[String]],
+    config: Expr[ElementCodecConfig],
+  )(using Quotes): Expr[XmlEncoder[T]] =
+    '{XmlEncoder.fromElementEncoder[T]($localName, $namespace)(${deriveElementEncoderImpl(config)})}
 
   // PRODUCT
 
-  private def extractFieldCategory(using Quotes)(
-    classSymbol: quotes.reflect.Symbol,
-    fieldSymbol: quotes.reflect.Symbol,
-    fieldAnnotations: List[Expr[Any]]
-  ): FieldCategory = {
-    import quotes.reflect.*
-    fieldAnnotations
-      .collect {
-        case '{attr()}    => FieldCategory.attribute
-        case '{text()}    => FieldCategory.text
-        case '{default()} => FieldCategory.default
-      } match {
-      case Nil            => FieldCategory.element
-      case List(category) => category
-      case categories =>
-        val categoryAnnotations =
-          categories.collect {
-            case FieldCategory.attribute => "@attr"
-            case FieldCategory.text => "@text"
-            case FieldCategory.default => "@default"
-          }.mkString(", ")
-
-        report.throwError(
-          s"""
-             |Case class field cannot have more than one category annotation (@attr, @text or @default).
-             |Field '${fieldSymbol.name}' in case class '${classSymbol.name}' has ${categories.size}: $categoryAnnotations
-             |""".stripMargin
-        )
-    }
-  }
-
-  private def extractFieldXmlName(using Quotes)(
-    config: Expr[ElementCodecConfig],
-    classSymbol: quotes.reflect.Symbol,
-    fieldSymbol: quotes.reflect.Symbol,
-    fieldAnnotations: List[Expr[Any]],
-    fieldCategory: FieldCategory,
-  ): Expr[String] = {
-    import quotes.reflect.*
-    (fieldAnnotations.collect {case '{renamed($a)} => a } match {
-      case Nil        => None
-      case List(name) => Some(name)
-      case names =>
-        val renamedAnnotations = names.map(name => s"@renamed(${name.asTerm.show})").mkString(", ")
-        report.throwError(
-          s"""
-             |Case class field cannot have more than one @renamed annotation.
-             |Field '${fieldSymbol.name}' in case class '${classSymbol.name}' has ${names.size}: $renamedAnnotations
-             |""".stripMargin
-        )
-    }).getOrElse(fieldCategory match {
-      case FieldCategory.element   => '{${config}.transformElementNames(${Expr(fieldSymbol.name)})}
-      case FieldCategory.attribute => '{${config}.transformAttributeNames(${Expr(fieldSymbol.name)})}
-      case _                       => Expr(fieldSymbol.name)
-    })
-  }
-
-  private def extractFeildNamespace(using Quotes)(
-    config: Expr[ElementCodecConfig],
-    classSymbol: quotes.reflect.Symbol,
-    fieldSymbol: quotes.reflect.Symbol,
-    fieldAnnotations: List[Expr[Any]],
-    fieldCategory: FieldCategory,
-  ): Expr[Option[String]] = {
-    import quotes.reflect.*
-    fieldAnnotations.collect {
-      case '{xmlns($namespace: b)} => '{Some(summonInline[Namespace[b]].getNamespace)}
-    } match {
-      case Nil => fieldCategory match {
-        case FieldCategory.element   => '{${config}.elementsDefaultNamespace}
-        case FieldCategory.attribute => '{${config}.attributesDefaultNamespace}
-        case _ => '{None}
-      }
-      case List(namespace) => namespace
-      case namespaces =>
-        val xmlnsAnnotations =
-          fieldAnnotations
-            .collect {
-              case '{xmlns($namespace)} => s"@xmlns(${namespace.asTerm.show})"
-            }
-            .mkString(", ")
-        report.throwError(
-          s"""
-             |Case class field cannot have more than one @xmlns annotation.
-             |Field '${fieldSymbol.name}' in case class '${classSymbol.name}' has ${namespaces.size}: $xmlnsAnnotations
-             |""".stripMargin
-        )
-    }
-  }
-
-
-  private def extractCaseClassFields[T: Type](config: Expr[ElementCodecConfig])(using Quotes): List[CaseClassField] = {
-    import quotes.reflect.*
-
-    val classTypeRepr = TypeRepr.of[T]
-    val classSymbol = classTypeRepr.typeSymbol
-
-    val fields = classSymbol.primaryConstructor.paramSymss.flatten.map { fieldSymbol =>
-      val fieldAnnotations = fieldSymbol.annotations.map(_.asExpr)
-      val fieldCategory    = extractFieldCategory(classSymbol, fieldSymbol, fieldAnnotations)
-      val fieldXmlName     = extractFieldXmlName(config, classSymbol, fieldSymbol, fieldAnnotations, fieldCategory)
-      val fieldNamespace   = extractFeildNamespace(config, classSymbol, fieldSymbol, fieldAnnotations, fieldCategory)
-      CaseClassField()(fieldSymbol.name, fieldXmlName, fieldNamespace, classTypeRepr.memberType(fieldSymbol), fieldCategory)
-    }
-    val textCount    = fields.count(_.category == FieldCategory.text)
-    val defaultCount = fields.count(_.category == FieldCategory.default)
-    if (textCount > 1)
-      report.throwError(
-        s"""
-           |Case class cannot have more than one field with @text annotation.
-           |Case class '${classSymbol.name}' has $textCount
-           |""".stripMargin
-      )
-    if (defaultCount > 1)
-      report.throwError(
-        s"""
-           |Case class cannot have more than one field with @default annotation.
-           |Case class '${classSymbol.name}' has $defaultCount
-           |""".stripMargin
-      )
-    fields
-  }
-
   private def encodeAttributes[T: Type](using Quotes)(
-    fields: List[CaseClassField],
+    fields: List[ProductTypeField],
     sw: Expr[PhobosStreamWriter],
     a: Expr[T]
   ): Expr[List[Unit]] = {
@@ -169,7 +58,7 @@ object encoder {
     val classTypeRepr = TypeRepr.of[T]
     val classSymbol = classTypeRepr.typeSymbol
     Expr.ofList(fields.map{ field =>
-      field.paramType.asType match {
+      field.typeRepr.asType match {
         case '[t] => '{
           summonInline[AttributeEncoder[t]].encodeAsAttribute(
             ${Select(a.asTerm, classSymbol.declaredField(field.localName)).asExprOf[t]},
@@ -183,7 +72,7 @@ object encoder {
   }
 
   private def encodeText[T: Type](using Quotes)(
-    fields: List[CaseClassField],
+    fields: List[ProductTypeField],
     sw: Expr[PhobosStreamWriter],
     a: Expr[T]
   ): Expr[List[Unit]] = {
@@ -192,7 +81,7 @@ object encoder {
     val classSymbol = classTypeRepr.typeSymbol
 
     Expr.ofList(fields.map { field =>
-      field.paramType.asType match {
+      field.typeRepr.asType match {
         case '[t] => '{
           summonInline[TextEncoder[t]]
             .encodeAsText(${Select(a.asTerm, classSymbol.declaredField(field.localName)).asExprOf[t]}, $sw)
@@ -202,7 +91,7 @@ object encoder {
   }
 
   private def encodeElements[T: Type](using Quotes)(
-    fields: List[CaseClassField],
+    fields: List[ProductTypeField],
     sw: Expr[PhobosStreamWriter],
     a: Expr[T]
   ): Expr[List[Unit]] = {
@@ -211,7 +100,7 @@ object encoder {
     val classSymbol = classTypeRepr.typeSymbol
 
     Expr.ofList(fields.map { field =>
-      field.paramType.asType match {
+      field.typeRepr.asType match {
         case '[t] => '{
           summonInline[ElementEncoder[t]].encodeAsElement(
             ${Select(a.asTerm, classSymbol.declaredField(field.localName)).asExprOf[t]},
@@ -225,11 +114,11 @@ object encoder {
   }
 
 
-  private def deriveProductImpl[T: Type](config: Expr[ElementCodecConfig])(using Quotes): Expr[ElementEncoder[T]] = {
+  private def deriveProduct[T: Type](config: Expr[ElementCodecConfig])(using Quotes): Expr[ElementEncoder[T]] = {
     import quotes.reflect.*
     val classTypeRepr = TypeRepr.of[T]
     val classSymbol = classTypeRepr.typeSymbol
-    val fields = extractCaseClassFields[T](config)
+    val fields = extractProductTypeFields[T](config)
 
     val groups = fields.groupBy(_.category)
 
@@ -251,40 +140,9 @@ object encoder {
 
   // SUM
 
-  private def extractChildXmlName(using Quotes)(
-    config: Expr[ElementCodecConfig],
-    traitSymbol: quotes.reflect.Symbol,
-    childSymbol: quotes.reflect.Symbol,
-  ): Expr[String] = {
-    import quotes.reflect.*
-    childSymbol.annotations.map(_.asExpr).collect { case '{discriminator($a)} => a } match {
-      case Nil        => '{$config.transformConstructorNames(${Expr(childSymbol.name)})}
-      case List(name) => name
-      case names =>
-        val discriminatorAnnotations = names.map(name => s"@discriminator(${name.show})").mkString(", ")
-        report.throwError(
-          s"""
-             |Sealed trait child cannot have more than one @discriminator annotation.
-             |Child '${childSymbol.name}' of sealed trait '${traitSymbol.name}' has ${names.size}: $discriminatorAnnotations
-             |""".stripMargin
-        )
-    }
-  }
-
-  private def extractSealedTraitChildren[T: Type](config: Expr[ElementCodecConfig])(using Quotes): List[SealedTraitChild] = {
-    import quotes.reflect.*
-    val traitTypeRepr = TypeRepr.of[T]
-    val traitSymbol   = traitTypeRepr.typeSymbol
-
-    traitSymbol.children.map { childSymbol =>
-      val xmlName = extractChildXmlName(config, traitSymbol, childSymbol)
-      SealedTraitChild()(xmlName, TypeIdent(childSymbol).tpe)
-    }
-  }
-
   private def encodeChild[T: Type](using Quotes)(
     config: Expr[ElementCodecConfig],
-    child: SealedTraitChild,
+    child: SumTypeChild,
     childValue: Expr[T],
     sw: Expr[PhobosStreamWriter],
     localName: Expr[String],
@@ -303,7 +161,7 @@ object encoder {
     }
   }
 
-  private def deriveSumImpl[T: Type](config: Expr[ElementCodecConfig])(using Quotes): Expr[ElementEncoder[T]] = {
+  private def deriveSum[T: Type](config: Expr[ElementCodecConfig])(using Quotes): Expr[ElementEncoder[T]] = {
     import quotes.reflect.*
 
     '{
@@ -312,8 +170,8 @@ object encoder {
           ${
             Match(
               '{a}.asTerm,
-              extractSealedTraitChildren[T](config).map { child =>
-                child.subtypeType.asType match {
+              extractSumTypeChildren[T](config).map { child =>
+                child.typeRepr.asType match {
                   case '[t] =>
                     val childValueSymbol = Symbol.newBind(Symbol.spliceOwner, "child", Flags.EmptyFlags, TypeRepr.of[t])
                     val encode = encodeChild(config, child, Ref(childValueSymbol).asExprOf[t], 'sw, 'localName, 'namespaceUri)
@@ -326,35 +184,4 @@ object encoder {
       }
     }
   }
-
-  inline def deriveEncoder[T](inline config: ElementCodecConfig): ElementEncoder[T] =
-    ${deriveEncoderImpl('{config})}
-
-  def deriveEncoderImpl[T: Type](config: Expr[ElementCodecConfig])(using Quotes): Expr[ElementEncoder[T]] = {
-    import quotes.reflect.*
-
-    val tpe = TypeRepr.of[T]
-    val typeSymbol = tpe.typeSymbol
-    if (typeSymbol.flags.is(Flags.Case)) {
-      deriveProductImpl(config)
-    } else if (typeSymbol.flags.is(Flags.Sealed)) {
-      deriveSumImpl(config)
-    } else {
-      report.throwError(s"${typeSymbol} is not a case class or sealed trait")
-    }
-  }
-
-  inline def xml[T](
-    inline localName: String,
-    inline namespace: Option[String],
-    inline config: ElementCodecConfig
-  ): XmlEncoder[T] =
-    ${xmlImpl('{localName}, '{namespace}, '{config})}
-
-  def xmlImpl[T: Type](
-    localName: Expr[String],
-    namespace: Expr[Option[String]],
-    config: Expr[ElementCodecConfig],
-  )(using Quotes): Expr[XmlEncoder[T]] =
-    '{XmlEncoder.fromElementEncoder[T]($localName, $namespace)(${deriveEncoderImpl(config)})}
 }
--- modules/core/src/main/scala-3/ru/tinkoff/phobos/derivation/semiauto/package.scala
+++ modules/core/src/main/scala-3/ru/tinkoff/phobos/derivation/semiauto/package.scala
@@ -9,17 +9,17 @@ import scala.deriving.Mirror
 
 package object semiauto {
   inline def deriveElementEncoder[T]: ElementEncoder[T] =
-    encoder.deriveEncoder[T](ElementCodecConfig.default)
+    encoder.deriveElementEncoder[T](ElementCodecConfig.default)
   inline def deriveElementEncoderConfigured[T](config: ElementCodecConfig): ElementEncoder[T] =
-    encoder.deriveEncoder[T](config)
+    encoder.deriveElementEncoder[T](config)
   inline def deriveXmlEncoder[T](localName: String): XmlEncoder[T] =
-    encoder.xml[T](localName, None, ElementCodecConfig.default)
+    encoder.deriveXmlEncoder[T](localName, None, ElementCodecConfig.default)
   inline def deriveXmlEncoderConfigured[T](localName: String, config: ElementCodecConfig): XmlEncoder[T] =
-    encoder.xml[T](localName, None, config)
+    encoder.deriveXmlEncoder[T](localName, None, config)
   inline def deriveXmlEncoder[T, NS: Namespace](localName: String, ns: NS): XmlEncoder[T] =
-    encoder.xml[T](localName, Some(Namespace[NS].getNamespace), ElementCodecConfig.default)
+    encoder.deriveXmlEncoder[T](localName, Some(Namespace[NS].getNamespace), ElementCodecConfig.default)
   inline def deriveXmlEncoderConfigured[T, NS: Namespace](localName: String, ns: NS, config: ElementCodecConfig): XmlEncoder[T] =
-    encoder.xml[T](localName, Some(Namespace[NS].getNamespace), config)
+    encoder.deriveXmlEncoder[T](localName, Some(Namespace[NS].getNamespace), config)
   def deriveElementDecoder[T]: ElementDecoder[T]                                                              = null
   def deriveElementDecoderConfigured[T](config: ElementCodecConfig): ElementDecoder[T]                        = null
   def deriveXmlDecoder[T](localName: String): XmlDecoder[T]                                                   = null
